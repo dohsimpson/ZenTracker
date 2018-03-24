ecf51e1a2d51fbfbdefe4c767ba46333bb532ac1
dohsimpson <mydohsimpson@gmail.com>
2018-03-23 23:02:47 -0400

    bump fix version

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ class Zentracker < Formula
   desc "Command Line Automatic Time Tracker for OSX"
   homepage "https://github.com/dohsimpson/ZenTracker"
   url "https://github.com/dohsimpson/ZenTracker/archive/master.zip"
-  version "0.0.1"
+  version "0.0.2"
   sha256 ""
 
   def install
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 brew services stop zentracker
+brew cleanup -s zentracker
 brew uninstall zentracker
 rm /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/zentracker.rb
